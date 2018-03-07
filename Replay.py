dab99332fd37df15c0a4cc1302bdf3c978cff07b
alexlitz <captainjacksparrow1999@gmail.com>
2018-03-07 12:41:18 -0500

    refinement

--- Replay.py
+++ Replay.py
@@ -1,5 +1,7 @@
 import torch
+from torch.utils.data.dataset import Dataset
 import gym
+from collections import namedtuple
 
 Transition = namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
 
@@ -14,30 +16,41 @@ class ReplayDataset(Dataset):
         self.full = False
         self.len = 0
         self.capacity = capacity
-        self.states = torch.FloatTensor(capacity, env.nS)
-        self.actions = torch.FloatTensor(capacity, env.nA)
+        stateDims = [capacity] + list(env.observation_space.shape)
+        self.states = torch.FloatTensor(*stateDims)
+        self.actions = torch.FloatTensor(capacity, env.action_space.n)
         self.rewards = torch.FloatTensor(capacity)
-        self.next_states = torch.FloatTensor(capacity, env.nS)
+        self.next_states = torch.FloatTensor(*stateDims)
         self.dones = torch.ByteTensor(capacity)
 
     def __len__(self):
         return self.len
 
-    def __insert__(self, i, t):
+    def __insert__(self, i, *args):
+        if isinstance(args[0], Transition):
+            t = args[0]
+        else:
+            t = Transition(args)
+
         self.states[i] = torch.Tensor([t.state])
         self.actions[i] = torch.Tensor([t.action])
         self.rewards[i] = float(t.reward)
         self.next_states[i] = torch.Tensor([t.next_state])
         self.dones[i] = int(t.done)
 
-
-    def __add__(self, t):
+    def __add__(self, *args):
+        if isinstance(args[0], Transition):
+            t = args[0]
+        else:
+            t = Transition(args)
+            
         if not self.full:
             self.insert(self.len, t)
             self.len += 1
             self.full = self.len==self.capacity
         else:
             self.insert(random.randint(0, self.len), t)
+
 
     def __getitem__(self, idx):
         return Transition(self.states[i], self.actions[i], self.rewards[i], 
